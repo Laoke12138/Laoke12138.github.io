86e78d1bd66e2001e846dedae58bf8274be32e05
2469844313@qq.com <2469844313@qq.com>
2024-12-29 20:20:04 +0800

    Site updated: 2024-12-29 20:20:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["read/2024年度总结—指条明路吧/"];function toRandomPost(){
+var posts=["read/2024年度总结—指条明路吧/","read/Pytorch入门-0/","read/pytorch入门/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
